7c89cb5541941d14caebf22a425acd872eef8a6b
vinidtm <vinidtm@hotmail.com>
2023-05-09 18:32:48 -0300

    alteração head

--- pages/Header.js
+++ pages/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import VideoPlayer from './VideoPlayer';
 import Comments from './Comments';
+import headline1 from './fotos/headline1'
 
 
 function Header () {
   return (
     <div>
         <div className="video-player-container">
+        <Image src={headline1} width={900}/>
         <VideoPlayer />
         </div>
         <Comments/>
