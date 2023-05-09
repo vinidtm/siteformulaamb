f60a21e53ed27609e529f817c36b87a5a2f02b9a
vinidtm <vinidtm@hotmail.com>
2023-05-09 19:25:00 -0300

    auteração css

--- pages/Header.js
+++ pages/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import VideoPlayer from './VideoPlayer';
 import Comments from './Comments';
+import Image from 'next/image';
 import headline1 from './fotos/head1.gif'
 
 
 function Header () {
   return (
     <div>
-        <Image src={headline1} width={900}/>
+        <div className='headline-container'>
+          <div className='.conteiner'>
+          <Image src={headline1} layout="responsive" maxWidth={100} />
+        </div>
+        </div>
         <div className="video-player-container">
         <VideoPlayer />
         </div>
