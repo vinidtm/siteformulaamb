24ae954215e9658c2e8c38ebcb3ff163732a1a9a
vinidtm <vinidtm@hotmail.com>
2023-05-09 20:27:35 -0300

    teste sub url

---
+++ pages/Formula_amb.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+function Formula_amb() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Formula_amb;
