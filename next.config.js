f424521493084fc90e5d49fba75d09adc6b05281
vinidtm <vinidtm@hotmail.com>
2023-05-16 18:45:29 -0300

    pagina teste

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/amb/:path*',
-          destination: '/Amb',
-        },
-      ];
-    },
-  };
+  async rewrites() {
+    return [
+      {
+        source: '/amb/:path*',
+        destination: '/Amb',
+      },
+    ];
+  },
+};
--- pages/AmbA.js
+++ pages/AmbA.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import Header from './Header';
 import Footer from './Footer';
+import VideoPlayer from './VideoPlayer';
+import Comments from './Comments';
+import Image from 'next/image';
+import cientista_r_v from './fotos/cientista_r_v.gif'
+
 
 function Amb() {
   return (
-    <div>
-      <Header />
+    <div className='fundo-verde'>
+      <div className='headline-container'>
+          <div className='.conteiner'>
+          <Image src={cientista_r_v} layout="responsive" maxWidth={100} />
+        </div>
+        </div>
+        <div className="video-player-container">
+        <VideoPlayer />
+        </div>
+        <Comments/>
       <Footer />
     </div>
   );
