196842b7dd729b87606c6cc8bd48c33789bf7883
vinidtm <vinidtm@hotmail.com>
2023-05-16 10:21:20 -0300

    fundo branco

--- pages/Header.js
+++ pages/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import VideoPlayer from './VideoPlayer';
 import Comments from './Comments';
 import Image from 'next/image';
-import headline2 from './fotos/head2.gif'
+import headline3 from './fotos/head3.gif'
 
 
 function Header () {
@@ -10,7 +10,7 @@ function Header () {
     <div>
         <div className='headline-container'>
           <div className='.conteiner'>
-          <Image src={headline2} layout="responsive" maxWidth={100} />
+          <Image src={headline3} layout="responsive" maxWidth={100} />
         </div>
         </div>
         <div className="video-player-container">
