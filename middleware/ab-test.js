0383f58c3d03df7b5a5654dead16f8e7c43a418d
Eliseudev96 <eliseuespada@gmail.com>
2023-05-18 11:13:42 -0300

    update

--- middleware/ab-test.js
+++ middleware/ab-test.js
@@ -1,5 +1,12 @@
 export default function abTest(req, res, next) {
-  const experiment = Math.random() < 0.5 ? 'AmbA' : 'AmbB';
+  let experiment;
+
+  if (req.cookies['abTest']) {
+    experiment = req.cookies['abTest'];
+  } else {
+    experiment = Math.random() < 0.5 ? 'AmbA' : 'AmbB';
+    res.cookie('abTest', experiment, { maxAge: 900000, httpOnly: true });
+  }
 
   req.url = `/${experiment}`;
 
